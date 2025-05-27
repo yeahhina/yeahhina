b7fb4f9f0cee80edd86873fe5f8bc390051c9c8a
Mumtahina <hina.yeasmin19@gmail.com>
2025-05-27 23:29:29 +0100

    startt

---
+++ app/view/loginStyle.tsx
@@ -0,0 +1,45 @@
+import {StyleSheet} from "react-native";
+
+const LoginStyle = StyleSheet.create({
+    container:
+    {
+        backgroundColor: '#687DA8',
+        flex:1,
+        width: "100%",
+        justifyContent: "center",
+        alignItems:"center",
+        gap:60
+    },
+    signInContainer:
+    {
+        gap:12,
+    },
+    input:
+    {
+        color:"#FFF",
+        width:256,
+        borderBottomWidth:1,
+        borderBottomColor:"#FFF",
+        fontFamily:"Inter",
+        fontSize: 16
+    },
+    button:
+    {
+        width: 256,
+        borderRadius: 10,
+        borderWidth: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "#FFF",
+        marginTop: 26
+
+    },
+    buttonText:
+    {
+        color:"#FFF",
+        fontFamily:"Inter",
+        fontSize:16,
+        padding: 9
+    }
+})
+export default LoginStyle;
